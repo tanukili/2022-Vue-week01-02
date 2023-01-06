6786ec466e000e58200e0b76dfdf93e4508cf49d
tanukili <108317269+tanukili@users.noreply.github.com>
2023-01-07 01:47:09 +0800

    Update 2023-01-06T17:47:06.063Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -41,32 +41,18 @@ var products = [{
   imageUrl: "https://images.unsplash.com/photo-1505253149613-112d21d9f6a9?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDZ8fGNha2V8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60",
   imagesUrl: ["https://images.unsplash.com/flagged/photo-1557234985-425e10c9d7f1?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTA5fHxjYWtlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60", "https://images.unsplash.com/photo-1540337706094-da10342c93d8?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDR8fGNha2V8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60"]
 }];
-var productList = document.querySelector('.productList');
-var productNum = document.querySelector('.productNum'); //渲染產品列表
-
-function renderProductList() {
-  var template = '';
-  products.forEach(function (item) {
-    template = template + "<tr>\n      <td width=\"150\">".concat(item.title, "</td>\n      <td width=\"120\">\n        ").concat(item.origin_price, "\n      </td>\n      <td width=\"120\">\n        ").concat(item.price, "\n      </td>\n      <td width=\"150\">\n      <span class=\"text-success\">\u555F\u7528</span>\n        <span>\u672A\u555F\u7528</span>\n      </td>\n      <td width=\"120\">\n        <button type=\"button\" class=\"btn btn-primary\">\u67E5\u770B\u7D30\u7BC0</button>\n      </td>\n    </tr>");
-  });
-  productList.innerHTML = template;
-  productNum.innerHTML = "\u76EE\u524D\u6709 <span>".concat(products.length, "</span> \u9805\u7522\u54C1");
-}
-
-renderProductList(); //渲染產品細節
-
-var productDetail = document.querySelector('.productDetail');
-
-function renderProductDetail(id) {
-  var data = products.filter(function (item) {
-    return item.id === id;
-  });
-  console.log(data[0].title);
-  var template = '';
-  template = "<template>\n    <div class=\"card mb-3\">\n      <img src=\"\" class=\"card-img-top primary-image\" alt=\"\u4E3B\u5716\">\n      <div class=\"card-body\">\n        <h5 class=\"card-title\">\n          ".concat(data.title, "\n          <span class=\"badge bg-primary ms-2\">{{  }}</span>\n        </h5>\n        <p class=\"card-text\">\u5546\u54C1\u63CF\u8FF0\uFF1A{{  }}</p>\n        <p class=\"card-text\">\u5546\u54C1\u5167\u5BB9\uFF1A{{  }}</p>\n        <div class=\"d-flex\">\n          <p class=\"card-text me-2\">{{  }}</p>\n          <p class=\"card-text text-secondary\"><del>{{  }}</del></p>\n        \u5143 / {{  }}\n        </div>\n      </div>\n    </div>\n    <template>\n      <img src=\"\" alt=\"\" class=\"images m-2\">\n    </template>\n  </template>\n  ");
-  productDetail.innerHTML = template;
-}
-
-;
-renderProductDetail('-L9tH8jxVb2Ka_DYPwng');
+Vue.createApp({
+  // 資料 (函式)
+  data: function data() {
+    return {
+      products: [],
+      tempProduct: {}
+    };
+  },
+  // 生命週期 (函式)
+  created: function created() {
+    // 元件生成時載入
+    this.products = products;
+  }
+}).mount('#app');
 //# sourceMappingURL=all.js.map
